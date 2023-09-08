bb905c5ed000e52eac8456ce29cfe449176fe139
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-09-08 19:46:50 -0300

    Add: função buscaVideo

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -26,9 +26,18 @@ async function criaVideo(titulo, descricao, url, imagem) {
 
 };
 
+async function buscaVideo(termoDeBusca) {
+
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = conexao.json();
+    return conexaoConvertida;
+
+};
+
 export const conectaAPI = {
 
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideo
 
 };
