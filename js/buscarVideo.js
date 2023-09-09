d5e77dd27b30d07d5a7a443509c0cdb1c480941a
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-09-09 11:04:25 -0300

    Add: busca de vídeo concluída

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,7 +1,26 @@
 import { conectaAPI } from "./conectaAPI.js";
+import constroiCard from "./mostrarVideos.js";
 
-async function buscarVideo() {
+async function buscarVideo(evento) {
 
-    const busca = await conectaAPI.buscaVideo();
+    evento.preventDefault();
+
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+    const busca = await conectaAPI.buscaVideo(dadosDePesquisa);
+
+    const lista = document.querySelector("[data-lista]");
+
+    while (lista.firstChild) {
+        
+        lista.removeChild(lista.firstChild);
+
+    }
+
+    busca.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
 
 };
+
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
+
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento));
